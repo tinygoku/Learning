92833061fa548a3924391946a431dd5ea46eb360
tinygoku <47263692+tinygoku@users.noreply.github.com>
2019-02-03 02:20:51 -0700

    Add files via upload

---
+++ a3/Math.py
@@ -0,0 +1,16 @@
+print("Hello! lets try something")
+first_number = int(input("Please enter first Number "))
+second_number = int(input("Please enter second Digit "))
+third_number = int(input("Please enter third number "))
+if first_number == second_number == third_number:
+    print("Don't just spam a key, idiot... ")
+sum = first_number + second_number + third_number
+multiply = first_number * second_number * third_number
+print("Addition of the entered numbers is: ", sum)
+print("Multiplication of the entered number is: ", multiply)
+
+
+
+
+
+
