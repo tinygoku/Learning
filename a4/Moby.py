576f1ec28737b44deac82dd5463402a8bfddf69d
Julian Andres <j.mena.vasq@gmail.com>
2019-02-13 20:18:06 -0700

    First attempt + Stringtheory

--- a4/Moby.py
+++ a4/Moby.py
@@ -1,7 +1,7 @@
 def moby_discount( x ):
     regular = (x * 1.05)
     membership = (x * 0.735)
-    print("Your total is %.2f", regular)
+    print("Your total is ", regular)
     print("Your total with a membership $", membership)
     print("you saved $ ", regular - membership)
 
