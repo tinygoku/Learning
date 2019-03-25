c5278f880ba570237c49b63872cc1738d91e16cc
Julian Andres <j.mena.vasq@gmail.com>
2019-03-24 22:08:39 -0600

    added appropiate formatting to get 2 decimals

--- a4/Moby.py
+++ a4/Moby.py
@@ -1,15 +1,14 @@
 def normal(price):
     return 1.05 * price
-
+#function figures out price + tax
 def discount(price):
     return (price * .85) * 1.05
-
+#function for price with discount
 price = float(input("Enter the value of your items: "))
 normal_price = normal(price)
 discount_price = discount(price)
 
-print("Your total is:$ ", normal_price)
-print("Total with Membership:$ ", discount_price)
-print("You saved:$", normal_price - discount_price)
+print("Your total is:${0:.2f}".format(normal_price))
+print("Total with Membership:${0:.2f}".format(discount_price))
+print("You saved:${0:.2f}".format(normal_price - discount_price))
 
-# decimals, only on print line.
