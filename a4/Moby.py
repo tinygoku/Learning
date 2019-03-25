e38ed41f6c1147160a3632565664d2f9acbb7421
Mike <phever@users.noreply.github.com>
2019-03-25 13:48:06 -0600

    change the formatting only!!!

--- a4/Moby.py
+++ a4/Moby.py
@@ -1,9 +1,11 @@
+# function figures out price + tax
 def normal(price):
     return 1.05 * price
-#function figures out price + tax
+
+# function for price with discount
 def discount(price):
     return (price * .85) * 1.05
-#function for price with discount
+
 price = float(input("Enter the value of your items: "))
 normal_price = normal(price)
 discount_price = discount(price)
@@ -11,4 +13,3 @@ discount_price = discount(price)
 print("Your total is:${0:.2f}".format(normal_price))
 print("Total with Membership:${0:.2f}".format(discount_price))
 print("You saved:${0:.2f}".format(normal_price - discount_price))
-
